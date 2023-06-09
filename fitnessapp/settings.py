2b7338c4ae83370e486269102aa6d1d723732192
nicoletc <nicoletc@stud.ntnu.no>
2023-06-09 23:56:47 +0300

    payment host in settings

--- fitnessapp/settings.py
+++ fitnessapp/settings.py
@@ -32,7 +32,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 
 # Application definition
-PAYMENT_HOST = 'http://localhost'
+PAYMENT_HOST = 'http://34.65.44.220'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
